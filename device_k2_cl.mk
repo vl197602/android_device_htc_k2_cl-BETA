8ef8348a193060f7b319b6e890741ba842601dbc
Austin Widner <austinw125@gmail.com>
2013-10-13 12:11:21 -0700

    Update device_k2_cl.mk

--- device_k2_cl.mk
+++ device_k2_cl.mk
@@ -134,7 +134,7 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
 # Extra properties
 PRODUCT_PROPERTY_OVERRIDES += \
